413c2c34e7f84f8fba45f9c53d72a98fe517ec88
Troy Stribling <troy.stribling@gmail.com>
2025-01-02 18:56:34 -0600

    added notebook for first example

---
+++ lib/plots/__init__.py
@@ -0,0 +1,6 @@
+from lib.plots.dash import  (curve, comparison, stack, twinx, twinx_comparison, bar, 
+                             twinx_bar_line, hist, fcurve, fpoints, fbar, contour, contour_hist, scatter, comparison_stack,
+                             colored_scatter_contour, colored_scatter, fscatter, twinx_bar_line,
+                             fcurve_scatter_comparison, positive_negative_bar)
+
+from lib.plots.comp.axis import (PlotType)
---
+++ lib/plots/dash/__init__.py
@@ -0,0 +1,4 @@
+from lib.plots.dash.curve import (curve, comparison, stack, twinx, twinx_comparison, bar, 
+                                  twinx_bar_line, hist, fcurve, fpoints, fscatter, fbar, scatter,
+                                  comparison_stack, fcurve_scatter_comparison, positive_negative_bar)
+from lib.plots.dash.surface import contour, contour_hist, colored_scatter_contour, colored_scatter
